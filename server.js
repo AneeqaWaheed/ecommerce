da496bd9e527bbacdc453cf84a9af7486496af9f
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-04-09 23:36:12 +0500

    Database connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 //congigure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest objects
 const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //rest api
 app.get('/',(req,res)=>{
@@ -18,5 +27,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT,()=>{
-    console.log(`Server running on ${process.env.dev_mode}${PORT}`);
+    console.log(`Server running on ${process.env.dev_mode} mode on ${PORT}`);
 })
